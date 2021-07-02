6912387262ea9c0efb7f46da51c7ed0376de7931
ByeonJang <86823053+ByeonJang@users.noreply.github.com>
2021-07-02 15:13:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,19 @@
 const CONFIG = {
-    introTitle: 'Hi Dúi của em à!',
-    introDesc: `Trái đất vốn lạ thường
-    
-    Mà sao em cứ đi nhầm đường
-    
-    Lạc vào tim anh lẻ loi
-    
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Anh có đang cần một cái ôm không nạ 😙',
-    desc: 'Em nhớ Dúi của em lắm nha. Phải mau về với em đó',
+    introTitle: 'Xin chào, Dúi của emm!',
+    introDesc: `Chỉ cần ngồi với anh chút thôi
+    Được không?
+    Nhìn vào đôi mắt anh thấy sao?
+    Một thế giới màu hồng`,
+    btnIntro: 'Em yêu anh',
+    title: 'Anh có đang cần một cái ôm không nạ😙',
+    desc: 'Em rất nhớ anh. CHúng ta mau gặp lại nhau nha! ',
     btnYes: 'Cần lắm <33',
     btnNo: 'Cực kỳ cần nhaaa :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
     btnReply: 'Gửi cho em <3',
-    reply: 'Yêu thì yêu không yêu thì yêu <333',
-    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Khi mọi thứ ổn hơn chúng ta cùng nhau đi chơi nhé!',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/byeonjang/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3',
+    mess: 'Em biết mà 🥰. Em cũng yêu anh rất nhiều 😘😘',
+    messDesc: 'Khi nào mọi chuyện ổn hơn chúng ta đi chơi nhé!',
+    btnAccept: 'Okii lun <3',
+    messLink: 'https://www.facebook.com/byeonjang/' //link mess của bạn. VD: https://www.facebook.com/byeonjang/
 }
