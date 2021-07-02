47d6f8a0c7b1b25441aaccbfa67f7d7721fda99d
ByeonJang <86823053+ByeonJang@users.noreply.github.com>
2021-07-02 15:10:39 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'Hi Dúi của em à!',
+    introDesc: `Trái đất vốn lạ thường
+    
+    Mà sao em cứ đi nhầm đường
+    
+    Lạc vào tim anh lẻ loi
+    
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Anh có đang cần một cái ôm không nạ 😙',
+    desc: 'Em nhớ Dúi của em lắm nha. Phải mau về với em đó',
+    btnYes: 'Cần lắm <33',
+    btnNo: 'Cực kỳ cần nhaaa :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Yêu thì yêu không yêu thì yêu <333',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
+    messDesc: 'Khi mọi thứ ổn hơn chúng ta cùng nhau đi chơi nhé!',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/byeonjang/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
